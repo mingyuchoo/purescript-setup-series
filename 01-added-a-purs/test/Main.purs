f79392dee281ea7d2647693e703dbabc14cd837c
mingyuchoo <mingyuchoo@gmail.com>
2021-10-24 10:41:17 +0900

    modified tests

--- 01-added-a-purs/test/Main.purs
+++ 01-added-a-purs/test/Main.purs
@@ -1,11 +1,10 @@
 module Test.Main where
 
 import Prelude
+import Test.Assert (assert)
 
-import Effect (Effect)
-import Effect.Class.Console (log)
+import Euler (answer)
 
-main :: Effect Unit
 main = do
-  log "🍝"
-  log "You should add some tests."
+    assert (answer == 233168)
+
