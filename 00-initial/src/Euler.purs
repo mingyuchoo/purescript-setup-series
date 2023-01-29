239c63a42ac64717404ee3e80366055caa264138
mingyuchoo <mingyuchoo@gmail.com>
2023-01-29 17:56:10 +0900

    updated

--- 00-initial/src/Euler.purs
+++ 00-initial/src/Euler.purs
@@ -5,9 +5,9 @@ import Prelude
 import Data.List (range, filter)
 import Data.Foldable (sum)
 
-ns = range 0 999
-
-multiples = filter (\n -> mod n 3 == 0 || mod n 5 == 0) ns
 
 answer = sum multiples
 
+multiples = filter (\n -> mod n 3 == 0 || mod n 5 == 0) ns
+
+ns = range 0 999
